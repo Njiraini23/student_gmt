a9e41aca50f77aad298d18d60e3f84fbea6c425e
Njiraini23 <njiraini88@gmail.com>
2024-03-31 16:29:33 +0300

    routes for the teachers model

--- resources/views/teachers/show.blade.php
+++ resources/views/teachers/show.blade.php
@@ -3,14 +3,14 @@
  
  
 <div class="card">
-  <div class="card-header">Students Page</div>
+  <div class="card-header">Teachers Page</div>
   <div class="card-body">
    
  
         <div class="card-body">
-        <h5 class="card-title">Name : {{ $students->name }}</h5>
-        <p class="card-text">Address : {{ $students->adress }}</p>
-        <p class="card-text">Mobile : {{ $students->mobile }}</p>
+        <h5 class="card-title">Name : {{ $teachers->name }}</h5>
+        <p class="card-text">Address : {{ $teachers->adress }}</p>
+        <p class="card-text">Mobile : {{ $teachers->mobile }}</p>
   </div>
        
     </hr>
